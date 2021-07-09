d380ae07e79eb5c73f17efdc80601fcfe3fbe046
Shakiba448 <shakiba448@gmail.com>
2021-07-09 12:45:31 +0600

    Done With it

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  Image,
-  SafeAreaView,
-  TouchableNativeFeedback,
-  View,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Press");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello React Native
-      </Text>
-      <TouchableNativeFeedback onPress={handlePress}>
-        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}>
-          {/* <Image
-            // blurRadius={10}
-            fadeDuration={1000}
-            source={{
-              width: 200,
-              height: 300,
-              uri: "https://picsum.photos/200/300",
-            }}
-          /> */}
-        </View>
-      </TouchableNativeFeedback>
+      <Button color="orange" title="Click me" onPress={handlePress}></Button>
     </SafeAreaView>
   );
 }
