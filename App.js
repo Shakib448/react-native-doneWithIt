b3231cef2d6fbe307b95f670d3fad3ab3f9a6ce1
Shakiba448 <shakiba448@gmail.com>
2021-07-09 12:38:31 +0600

    Done With it

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Press");
@@ -9,15 +15,17 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlePress}>
         Hello React Native
       </Text>
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableHighlight onPress={handlePress}>
+        <Image
+          // blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
