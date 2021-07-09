3abe10a9eed482eb11efb1fe7a84a2de1bbba2c9
Shakiba448 <shakiba448@gmail.com>
2021-07-09 16:50:47 +0600

    Done With it

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Platform, StatusBar, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }}></View>
-      <View style={{ backgroundColor: "gold", flex: 1 }}></View>
-      <View style={{ backgroundColor: "tomato", flex: 1 }}></View>
+    <View style={styles.container}>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      ></View>
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+      <View
+        style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+      ></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "row",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
