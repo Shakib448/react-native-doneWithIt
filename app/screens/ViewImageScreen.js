7ea82e06a21606f6c33f7e45005c2e39c8133f8e
Shakiba448 <shakiba448@gmail.com>
2021-07-10 09:47:10 +0600

    Done With it

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
 
 const ViewImageScreen = () => {
   return (
@@ -21,7 +22,7 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
@@ -33,7 +34,7 @@ const styles = StyleSheet.create({
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
