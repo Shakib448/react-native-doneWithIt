c787c08505244ae3bc23f0a9b0ea9ab3e3a06099
Shakiba448 <shakiba448@gmail.com>
2021-07-10 19:48:20 +0600

    Done With it

--- App.js
+++ App.js
@@ -2,17 +2,9 @@ import React from "react";
 import { Platform, StatusBar, StyleSheet, View, Text } from "react-native";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1,
-    flexDirection: "row",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
