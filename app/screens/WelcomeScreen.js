e72c75627578f22cd2e39627f04075987400464f
Shakiba448 <shakiba448@gmail.com>
2021-07-10 09:19:57 +0600

    Done With it

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View } from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.loginBtn}></View>
+      <View style={styles.registerBtn}></View>
+    </ImageBackground>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginBtn: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerBtn: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
