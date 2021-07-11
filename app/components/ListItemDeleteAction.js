774c794fd10667fbabe60bfa5bbb3bf83d73b4f0
Shakiba448 <shakiba448@gmail.com>
2021-07-11 17:40:43 +0600

    Done With it

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const ListItemDeleteAction = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
+    </View>
+  );
 };
 
 export default ListItemDeleteAction;
@@ -12,5 +17,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.danger,
     width: 70,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, View } from "react-native";
+import { FlatList } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "./../components/Screen";
 import ListIemSeparator from "./../components/ListIemSeparator";
