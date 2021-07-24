391b421e9bdc991ac9729948841aabcd04866ae4
Shakiba448 <shakiba448@gmail.com>
2021-07-24 17:35:31 +0600

    Done With it

--- app/api/client.js
+++ app/api/client.js
@@ -2,8 +2,4 @@ import { create } from "apisauce";
 
 const apiClient = create({ baseURL: "http://192.168.0.14:9000/api" });
 
-apiClient.get("/listings").then((response) => {
-  if (!response.ok) {
-    response.problem;
-  }
-});
+export default apiClient;
