963527f72ba2898e578bf43f70fd862de0dde1e1
Shakiba448 <shakiba448@gmail.com>
2021-07-28 14:29:45 +0600

    DoneWithIt

--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,18 @@
 import { create } from "apisauce";
+import cache from "../utils/cache";
 
 const apiClient = create({ baseURL: "http://192.168.0.111:9000/api" });
+
+const get = apiClient.get;
+
+apiClient.get = async (url, params, axiosConfig) => {
+  const res = await get(url, params, axiosConfig);
+  if (res.ok) {
+    cache.store(url, res.data);
+    return res;
+  }
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : res;
+};
 
 export default apiClient;
