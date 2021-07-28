db666dda168afc2f6bd631ca1b09e4008656a9cf
Shakiba448 <shakiba448@gmail.com>
2021-07-28 10:47:19 +0600

    DoneWithIt

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import NetInfo from "@react-native-community/netinfo";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  NetInfo.fetch().then((netinfo) => console.log(netinfo));
+  return null;
 }
+
+// <NavigationContainer theme={navigationTheme}>
+//   <AppNavigator />
+// </NavigationContainer>
