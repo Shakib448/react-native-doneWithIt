449e7809d4ad22c844c1a1499bf70661251d4720
Shakiba448 <shakiba448@gmail.com>
2021-08-02 09:26:20 +0600

    WIP on Header

--- app/auth/storage.js
+++ app/auth/storage.js
@@ -32,4 +32,4 @@ const getUser = async () => {
   return token ? jwtDecode(token) : null;
 };
 
-export default { getUser, removeToken, storeToken };
+export default { getToken, getUser, removeToken, storeToken };
